c3130b5008ebcce22261aa1ef293aecf32d3eb1f
Noob5000x <dxnlqzn@gmail.com>
2025-05-05 14:30:19 -0700

    Update client.py

--- client.py
+++ client.py
@@ -12,7 +12,10 @@ except socket.error as e:
     exit()
 msg = ""
 while True: ## will keep going to write messages back and forth until msg == 0
-    msg = input("Message Database (0 to quit, 1-3 to ask database queries): ")
+    msg = input("""Message Database (0 to quit, 1-3 to ask database queries):
+1.) What is the average moisture inside my kitchen fridge in the past three hours?
+2.) What is the average water consumption per cycle in my smart dishwasher?
+3.) Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?\n""")
     print("Client message to database:")
     if msg == "1":
         print("What is the average moisture inside my kitchen fridge in the past three hours?")
@@ -22,13 +25,14 @@ while True: ## will keep going to write messages back and forth until msg == 0
         print("Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?")
     elif msg == "0":
         print("Client closes.")
+        break
     else:
         print("Sorry, this query cannot be processed. Please try one of the following: [1, 2, 3].")
         continue
     myTCPSocket.send(bytearray(str(msg), encoding='utf-8')) ## sends mgs to server in utf-8
     serverResponse = myTCPSocket.recv(1024)
     if serverResponse:
-        print(f"Server: {serverResponse.decode('utf-8')}") ## decodes and prints server response
+        print(f"Server: {serverResponse.decode('utf-8')}\n") ## decodes and prints server response
     else:
         print("Server closed.")
         break
